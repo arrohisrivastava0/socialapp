7d933b43dd55e03cb35e080b913549b6101a89d9
arrohisrivastava <arrohisrivastava0@gmail.com>
2024-12-29 20:20:33 +0530

    Fix others' profile

--- lib/pages/others_profile_page.dart
+++ lib/pages/others_profile_page.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
@@ -5,6 +7,8 @@ import 'package:flutter/material.dart';
 import 'package:socialapp/pages/user_profile_page.dart';
 
 import '../components/my_button.dart';
+import '../components/wall_post_tile.dart';
+import 'connections_list_page.dart';
 
 class OthersProfilePage extends StatefulWidget {
   final String userId;
@@ -19,6 +23,10 @@ class _OthersProfilePageState extends State<OthersProfilePage> {
   bool isConnection= false;
   late String currentUserId;
   String userName= "Loading...";
+  String name="--";
+  String bio="--";
+  int connectionCount=0;
+  StreamSubscription? connectionListener;
 
   @override
   void initState() {
@@ -26,17 +34,41 @@ class _OthersProfilePageState extends State<OthersProfilePage> {
 
     // Get the current user's ID
     currentUserId = FirebaseAuth.instance.currentUser!.uid;
-    _fetchUserData();
+    fetchUserData();
     // Check the connection status
     _checkConnectionStatus();
   }
 
-  void _fetchUserData() async {
-    final userDoc = await FirebaseFirestore.instance.collection("Users").doc(widget.userId).get();
-    if (userDoc.exists) {
-      setState(() {
-        userName = userDoc.data()!['username'] ?? "Unknown User";
-      });
+  Future<void> fetchUserData() async {
+    try {
+      DocumentSnapshot<Map<String, dynamic>> snapshot = await FirebaseFirestore.instance
+          .collection("Users")
+          .doc(widget.userId)
+          .get();
+
+      if (snapshot.exists) {
+        Map<String, dynamic>? user = snapshot.data();
+        if (user != null && mounted) {
+          setState(() {
+            userName = user['username'] ?? "Unknown User";
+            name=user['name'];
+            bio=user['bio'];
+            connectionCount=user['num_connections'];
+          });
+        }
+      } else {
+        if (mounted) {
+          setState(() {
+            userName = "User not found";
+          });
+        }
+      }
+    } catch (e) {
+      if (mounted) {
+        setState(() {
+          userName = "Error loading user";
+        });
+      }
     }
   }
 
@@ -54,7 +86,37 @@ class _OthersProfilePageState extends State<OthersProfilePage> {
     }
   }
 
+  void startListeningToConnectionCount() {
+    connectionListener = FirebaseFirestore.instance
+        .collection("Connections")
+        .doc(widget.userId)
+        .snapshots()
+        .listen((snapshot) {
+      if (snapshot.exists) {
+        final connections = List<String>.from(snapshot.data()?['to'] ?? []);
+        setState(() {
+          connectionCount = connections.length;
+        });
+      } else {
+        setState(() {
+          connectionCount = 0;
+        });
+      }
+    });
+  }
 
+  Stream<List<Map<String, dynamic>>> fetchFeedPosts() {
+    final currentUserId = FirebaseAuth.instance.currentUser!.uid;
+    // Fetch posts based on connections
+    return FirebaseFirestore.instance
+        .collection('Posts')
+        .where('userId', isEqualTo: currentUserId)
+    // .orderBy('timestamp', descending: true)
+        .snapshots()
+        .map((querySnapshot) {
+      return querySnapshot.docs.map((doc) => doc.data()).toList();
+    });
+  }
 
   void _toggleConnectionStatus() async {
     final connectionDocRef = FirebaseFirestore.instance
@@ -103,8 +165,9 @@ class _OthersProfilePageState extends State<OthersProfilePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
 
       ),
-      body: FutureBuilder<DocumentSnapshot<Map<String, dynamic>>>(
-        future: FirebaseFirestore.instance.collection("Users").doc(widget.userId).get(),
+
+      body: StreamBuilder<DocumentSnapshot<Map<String, dynamic>>>(
+        stream: FirebaseFirestore.instance.collection("Users").doc(widget.userId).snapshots(),
         builder: (context, snapshot){
           if(snapshot.connectionState==ConnectionState.waiting){
             return const Center(
@@ -112,31 +175,108 @@ class _OthersProfilePageState extends State<OthersProfilePage> {
             );
           }
           else if(snapshot.hasData){
-            final user = snapshot.data!.data();
-            if (user == null) {
-              return const Center(child: Text("User not found"));
-            }
+            Map<String, dynamic>? user=snapshot.data!.data();
+            userName=user!['username'];
+            return SingleChildScrollView(
+              physics: BouncingScrollPhysics(),
+              child: Column(
+                children: [
+                  SizedBox(height: 30,),
+                  CircleAvatar(
+                    backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+                    radius: 50,
+                    child: Text(user!['name'][0].toUpperCase(), style: TextStyle(fontSize: 30),),
+                  ),
+                  SizedBox(height: 10,),
+                  Text(user!['name'], style: TextStyle(fontSize: 25),),
+                  Text(user!['bio'], style: TextStyle(fontSize: 17),),
+                  SizedBox(height: 30,),
+                  Text("$connectionCount connections"),
+                  const SizedBox(height: 30),
+                  MyButton(
+                    onTap: _toggleConnectionStatus,
+                    btnText: isConnection ? "Connected" : "Connect",
+                  ),
+                  const SizedBox(height: 40),
+                  Text("P O S T S", style: TextStyle(fontSize: 20),),
+                  Divider(
+                    color: Theme.of(context).colorScheme.inversePrimary,
+                    height: 6,
+                    thickness: 1,
+                    indent: 13,
+                    endIndent: 13,
+                  ),
+                  const SizedBox(height: 20),
+                  StreamBuilder<List<Map<String, dynamic>>>(
+                    stream: fetchFeedPosts(),
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return const Center(child: CircularProgressIndicator());
+                      } else if (snapshot.hasError) {
+                        return const Center(child: Text("Error loading posts."));
+                      } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                        return const Center(child: Text("You have no opinions"));
+                      }
 
-            return Column(
-              children: [
-                const SizedBox(height: 30),
-                Text(user['name'] ?? "No Name"),
-                Text(user['bio'] ?? "No Bio"),
-                const SizedBox(height: 30),
-                MyButton(
-                  onTap: _toggleConnectionStatus,
-                  btnText: isConnection ? "Connected" : "Connect",
-                ),
-              ],
+                      final posts = snapshot.data!;
+                      return Column(  // Use Column instead of ListView to list posts without scrolling
+                        children: posts.map((post) {
+                          return WallPostTile(
+                            postId: post['postId'] ?? 'Unknown Post ID',
+                            content: post['content'] ?? 'No content available',
+                            userId: post['username'] ?? 'Unknown user',
+                            timestamp: post['timestamp']?? 'No timestamp',
+                          );
+                        }).toList(),
+                      );
+                    },
+                  ),
+                ],
+
+              ),
             );
-
           }
 
           else{
-            return const Center(child: Text("Error Fetching Data"));
+            return Text("Error Fetching Data");
           }
         },
       ),
+
+      // body: FutureBuilder<DocumentSnapshot<Map<String, dynamic>>>(
+      //   future: FirebaseFirestore.instance.collection("Users").doc(widget.userId).get(),
+      //   builder: (context, snapshot){
+      //     if(snapshot.connectionState==ConnectionState.waiting){
+      //       return const Center(
+      //         child: CircularProgressIndicator(),
+      //       );
+      //     }
+      //     else if(snapshot.hasData){
+      //       final user = snapshot.data!.data();
+      //       if (user == null) {
+      //         return const Center(child: Text("User not found"));
+      //       }
+      //
+      //       return Column(
+      //         children: [
+      //           const SizedBox(height: 30),
+      //           Text(user['name'] ?? "No Name"),
+      //           Text(user['bio'] ?? "No Bio"),
+      //           const SizedBox(height: 30),
+      //           MyButton(
+      //             onTap: _toggleConnectionStatus,
+      //             btnText: isConnection ? "Connected" : "Connect",
+      //           ),
+      //         ],
+      //       );
+      //
+      //     }
+      //
+      //     else{
+      //       return const Center(child: Text("Error Fetching Data"));
+      //     }
+      //   },
+      // ),
 
     );
   }
