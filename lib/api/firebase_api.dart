2421d5ceca8fa97e2cbfe789d59298cc4988bd4d
arrohisrivastava <arrohisrivastava0@gmail.com>
2025-01-06 02:41:57 +0530

    Fix push notification

--- lib/api/firebase_api.dart
+++ lib/api/firebase_api.dart
@@ -1,14 +1,23 @@
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'dart:developer';
 
 class FirebaseApi{
-
+  // final _firebaseMessaging = FirebaseMessaging.instance;
   Future<void> initNotifications() async{
-    final _firebaseMessaging = FirebaseMessaging.instance;
-    await _firebaseMessaging.requestPermission();
-    final fcmToken = await _firebaseMessaging.getToken();
-    print('Token: $fcmToken');
 
-
+    try {
+      String? fcmToken = await FirebaseMessaging.instance.getToken();
+      if (fcmToken != null) {
+        log('Token: $fcmToken');
+        print('Token: $fcmToken');
+      } else {
+        log('FCM token is null');
+        print('FCM token is null');
+      }
+    } catch (e) {
+      log('Error fetching token: $e');
+      print('Error fetching token: $e');
+    }
 
   }
 }
