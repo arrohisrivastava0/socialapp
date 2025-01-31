4dfdac928538205e4f2ef5155456921e0c5d3904
arrohisrivastava <arrohisrivastava0@gmail.com>
2025-01-31 22:10:51 +0530

    Fix chat page

--- lib/pages/chat/chat_service.dart
+++ lib/pages/chat/chat_service.dart
@@ -1,32 +1,44 @@
+// import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
+//
+// class ChatService{
+//   final FirebaseFirestore _firestore= FirebaseFirestore.instance;
+//
+//   // Stream<List<Map<String, dynamic>>> getUserStream(){
+//   //   return _firestore.collection("Connections").snapshots().map((snapshot) {
+//   //     return snapshot.docs.map(toElement)
+//   //   });
+//   // }
+//
+//   Stream<DocumentSnapshot<Map<String, dynamic>>> fetchConnections() {
+//     final currentUserId=FirebaseAuth.instance.currentUser!.uid;
+//     return _firestore.collection("Connections").doc(currentUserId).snapshots();
+//     // try {
+//     //   final connectionDoc = await FirebaseFirestore.instance
+//     //       .collection('Connections')
+//     //       .doc(currentUserId)
+//     //       .get();
+//     //
+//     //   if (connectionDoc.exists) {
+//     //     // Assuming 'to' is a list of user IDs
+//     //     final List<dynamic> toList = connectionDoc.data()?['to'] ?? [];
+//     //     return toList.map((id) => {'id': id, 'timestamp': null}).toList();
+//     //   }
+//     // } catch (e) {
+//     //   print('Error fetching connections: $e');
+//     // }
+//     // return [];
+//   }
+// }
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
-class ChatService{
-  final FirebaseFirestore _firestore= FirebaseFirestore.instance;
-
-  // Stream<List<Map<String, dynamic>>> getUserStream(){
-  //   return _firestore.collection("Connections").snapshots().map((snapshot) {
-  //     return snapshot.docs.map(toElement)
-  //   });
-  // }
+class ChatService {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
   Stream<DocumentSnapshot<Map<String, dynamic>>> fetchConnections() {
-    final currentUserId=FirebaseAuth.instance.currentUser!.uid;
+    final currentUserId = FirebaseAuth.instance.currentUser!.uid;
     return _firestore.collection("Connections").doc(currentUserId).snapshots();
-    // try {
-    //   final connectionDoc = await FirebaseFirestore.instance
-    //       .collection('Connections')
-    //       .doc(currentUserId)
-    //       .get();
-    //
-    //   if (connectionDoc.exists) {
-    //     // Assuming 'to' is a list of user IDs
-    //     final List<dynamic> toList = connectionDoc.data()?['to'] ?? [];
-    //     return toList.map((id) => {'id': id, 'timestamp': null}).toList();
-    //   }
-    // } catch (e) {
-    //   print('Error fetching connections: $e');
-    // }
-    // return [];
   }
 }
